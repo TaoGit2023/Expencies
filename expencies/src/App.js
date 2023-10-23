669e32a2492ba6e57ed5d7c4bb558522895dd3c6
Tao <taogit2023@gmail.com>
2023-10-23 19:11:35 +1000

    expencises

--- expencies/src/App.js
+++ expencies/src/App.js
@@ -1,8 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
 import { Balance } from './components/Balance';
 import { IncomeExpense } from './components/IncomeExpense';
-import { Transaction } from './components/Transaction';
 import { AddTraction } from './components/AddTraction';
 import { GlobalProvider } from './context/GlobalState';
 import { TractionList } from './components/TransactionList';
--- expencies/src/components/AddTraction.jsx
+++ expencies/src/components/AddTraction.jsx
@@ -1,5 +1,39 @@
+import { useState, useContext } from "react"
+import {GlobalContext} from '../context/GlobalState'
+
 export const AddTraction = ()=>{
+    const {addTransaction} = useContext(GlobalContext)
+    const [text, setText] = useState('');
+    const [amount,setAmount] = useState(0);
+    const onSubmit = (e)=>{
+        e.preventDefault();
+        console.log('test')
+        const newTransaction={
+            id: Math.floor(Math.random()*10000000),
+            text,
+            amount:+amount
+        }
+        addTransaction(newTransaction)
+    }
     return(
-        <div>Add Transaction</div>
+        <div>
+            <h3> Add new transaction</h3>
+            <form id="form" onSubmit={onSubmit}>
+                <div>
+                    <input type="text"
+                    value={text} 
+                    onChange={(e)=>setText(e.target.value)}
+                    placeholder="Enter text..."/>
+                </div>
+                <div>
+                    <input type="number"
+                    value=""
+                    onChange={(e)=>setAmount(e.target.value)}
+                    placeholder=" Enter amount..."      
+                    />
+                </div>
+            </form>
+            <button className="btn"> Add Transaction</button>
+        </div>
     )
 }
